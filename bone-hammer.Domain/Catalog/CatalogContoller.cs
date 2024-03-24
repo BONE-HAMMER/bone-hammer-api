74be457dfa6fe677e6be578bd5ce4a0562e49ae9
abdoukeita687 <157330885+abdoukeita687@users.noreply.github.com>
2024-03-24 16:00:01 -0400

    modified:   bone-hammer.Domain/Catalog/CatalogContoller.cs

--- bone-hammer.Domain/Catalog/CatalogContoller.cs
+++ bone-hammer.Domain/Catalog/CatalogContoller.cs
@@ -10,7 +10,44 @@ namespace bone-hammer.Api.Controllers
         [HttpGet]
         public IActionResult GetItems()
         {
-            return Ok("hello world");
-        }
+           new Item("Shirt", "Ohio State shirt.", 29.99m),
+           new Item("Shorts", "Ohio State shorts." "Nike, 44.99m")
+        };
+        return Ok(items);
     }
+    [HttpGet("{id:int}")]
+    public IactionResult GetItem(int id)
+    {
+        var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
+        item.Id= id;
+        return Ok(item);
+    }
+
+    [HttpPost]
+    Public IactionResult Post(Item item)
+    {
+        return Created("/catalog/42", item);
+    
+    }
+    [HttpPost("{id:int}/rating")]
+    public IactionResult PostRating(int id, [FromBody] Rating rating)
+    {
+        var item =new Item("Shirt," "Ohio State Shirt." "Nike", 29.99m);
+        item.Id = id;
+        item.AddRating(rating);
+
+        return Ok(item);
 }
+[HttpPut("{id:int}")]
+public IActionResult Put(int id, Item item)
+{
+    return NoContent();
+}
+[HttpDelete("{id:int}")]
+public IActionResult Delete(int id)
+{
+    return NoContent();
+}
+
+}
+
